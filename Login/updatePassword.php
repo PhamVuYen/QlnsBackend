37696d807896ac6473827f728270950a0f1dc6dc
Pham Vu Yen <99155789+PhamVuYen@users.noreply.github.com>
2023-12-18 00:00:07 +0700

    feature hash encrypt password

--- Login/updatePassword.php
+++ Login/updatePassword.php
@@ -4,17 +4,16 @@ include '../Connect.php';
 include '../Staffs/Staff.php';
 
 if($_SERVER['REQUEST_METHOD']=='POST'){
-    $MaNV = $_POST['MaNV'];
-    $Password = $_POST['Password'];
+    $MaNV = isset($_POST['MaNV']) ? $_POST['MaNV'] : '';
+        $Password = isset($_POST['Password']) ? $_POST['Password'] : '';
     // Your login logic here
     // Verify email and password against your nhanvien table
     if(isset($_POST['MaNV']) && isset($_POST['Password'])){
-        $query = "UPDATE nhanvien SET Password = ' $Password' WHERE MaNV = '$MaNV' ";
+        $query = "UPDATE nhanvien SET Password = '$Password' WHERE MaNV = '$MaNV' ";
         $result = $conn->query($query);
         $conn->close();
-    }
-  
-}
 
+    }
+}
 
 ?>
